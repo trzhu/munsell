cf3585ce05d2337c3e5d28c722a038a5e4194e44
Trace Zhu <43766227+trzhu@users.noreply.github.com.>
2025-05-18 13:12:58 -0700

    skeleton

---
+++ js/main.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'OrbitControls';
+
+async function loadShader(url) {
+    const response = await fetch(url);
+    return await response.text();
+}
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x7f7f7f);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+document.getElementById('scene-container').appendChild(renderer.domElement);
+
+function resize() {
+  const container = document.getElementById('scene-container');
+  const width = container.clientWidth;
+  const height = container.clientHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+}
+window.addEventListener('resize', resize);
+resize();
+
+// load shaders (cooked rn)
+const vertexShader = await loadShader('./shaders/hsvsphere_frag.glsl');
+const fragmentShader = await loadShader('./shaders/hsvsphere_vert.glsl');
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// placeholder green cube
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+// placeholder cube
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// it rotat
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.y += 0.005;
+  renderer.render(scene, camera);
+}
+animate();
+
+// Button
+document.getElementById('button').addEventListener('click', () => {
+  console.log("the button doesnt do anything");
+});
