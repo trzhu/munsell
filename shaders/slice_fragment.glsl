25fc77964f041934857087c283c26802d64ac2e5
Trace Zhu <43766227+trzhu@users.noreply.github.com.>
2026-01-14 00:30:24 -0800

    oops

--- shaders/slice_fragment.glsl
+++ shaders/slice_fragment.glsl
@@ -6,8 +6,9 @@ uniform float useLighting;
 void main() {
     // convert xz location to polar coordinates
     vec2 xzPos = vObjectPosition.xz - vec2(0.5, 0.5);
-    // need a 180 degree offset 
-    float theta = mod(atan(xzPos.y, xzPos.x) + 3.14159265359, 6.28318530718);
+    // add pi to convert from [-pi, pi] to [0, 2pi] then add another pi for 180 deg offset
+    float theta = mod(atan(xzPos.y, xzPos.x) + 6.28318530718, 6.28318530718);
+
     float radius = length(xzPos) * 2.0;
     // y remains vertical coordinate for cylindrical coordinates
     float height = vObjectPosition.y;
